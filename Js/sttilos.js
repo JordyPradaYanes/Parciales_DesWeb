1034c82a13c4016455aa6d003301325cf95c6a64
JordyPradaYanes <jdpraday@ufpso.edu.co>
2024-01-14 01:01:02 -0500

    add hamburguer

--- Js/sttilos.js
+++ Js/sttilos.js
@@ -2,3 +2,25 @@ window.addEventListener("scroll",()=>{
     let nav = document.querySelector("nav");
     nav.classList.toggle("sticky", scrollY>0)
 })
+
+let enlaces_header = document.querySelectorAll(".enlaces-header")[0];
+let ico_hamburguer = true;
+
+document.querySelectorAll(".hamburgueri")[0].addEventListener("click", function(){
+    if(ico_hamburguer){
+        document.querySelectorAll(".hamburgueri")[0].style.color="#312BD9";
+        ico_hamburguer = false;
+    } 
+    else {
+        document.querySelectorAll(".hamburgueri")[0].style.color="#000";
+        ico_hamburguer = true;
+    }
+
+    enlaces_header.classList.toggle("hamburgueropen");
+
+})
+
+document.querySelectorAll(".enlaces-header")[0].addEventListener("click", function(){
+    enlaces_header.classList.toggle("hamburgueropen");
+    document.querySelectorAll(".hamburgueri")[0].style.color = "#000";
+})

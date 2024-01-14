691b7dd9c0ec4b4a1727545b20b4be528772589e
JordyPradaYanes <jdpraday@ufpso.edu.co>
2024-01-14 02:03:06 -0500

    add error correction

--- Js/sttilos.js
+++ Js/sttilos.js
@@ -5,15 +5,18 @@ window.addEventListener("scroll",()=>{
 
 let enlaces_header = document.querySelectorAll(".enlaces-header")[0];
 let ico_hamburguer = true;
+let main = document.querySelector("main");
 
 document.querySelectorAll(".hamburgueri")[0].addEventListener("click", function(){
     if(ico_hamburguer){
         document.querySelectorAll(".hamburgueri")[0].style.color="#312BD9";
         ico_hamburguer = false;
+        main.style.display="none";
     } 
     else {
         document.querySelectorAll(".hamburgueri")[0].style.color="#000";
         ico_hamburguer = true;
+        main.style.display="";
     }
 
     enlaces_header.classList.toggle("hamburgueropen");
